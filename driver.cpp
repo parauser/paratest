7787903872b54f8fcd83bee2ba87c094fc44b60e
parauser <village.david@gmail.com>
2018-02-12 07:13:46 -0800

    adding files for console-based version of Rullo (as found at: https://www.coolmath-games.com/0-rullo)

---
+++ driver.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "rullo.h"
+
+using namespace std;
+
+// Driver to start up the game of Rullo
+int main()
+{
+    Rullo game;
+    cout << "This is a version of the game Rullo" << endl;
+    game.play();
+    return 0;
+}
